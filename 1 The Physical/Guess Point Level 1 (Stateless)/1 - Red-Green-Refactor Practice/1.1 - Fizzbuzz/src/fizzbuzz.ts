a6e2eb0d16e3c104225485e89a41a2698fc5155a
Kaique Borges <rique.kaique@gmail.com>
2023-03-16 23:22:28 -0300

    green: given 1 should return '1'

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -1,9 +1,9 @@
 function fizzbuzz(x: number) {
   if (x % 3 === 0 && x % 5 === 0) return "FizzBuzz";
-
   if (x % 3 === 0) return "Fizz";
+  if (x % 5 === 0) return "Buzz";
 
-  return "Buzz";
+  return x.toString();
 }
 
 export default fizzbuzz;
