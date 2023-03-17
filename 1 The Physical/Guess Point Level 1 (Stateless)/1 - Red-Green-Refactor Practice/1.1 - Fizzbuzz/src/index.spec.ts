91b2c5f15a27b4b51b822d2d9723f49228360ba1
Kaique Borges <rique.kaique@gmail.com>
2023-03-16 23:08:40 -0300

    refactor: given 3 should return 'Fizz'

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -2,6 +2,10 @@ import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
   test("given 3 should return `Fizz`", () => {
-    expect(fizzbuzz(3)).toEqual("Fizz");
+    const expected = "Fizz";
+
+    const actual = fizzbuzz(3);
+
+    expect(actual).toEqual(expected);
   });
 });
